1326237deeb9b12fef1ba1ab9148d48b54c7670b
Ash Menon <ash@ashmenon.com>
2017-12-03 12:18:04 +0800

    Add support for SSL Configuration

--- src/Ratchet/Server/IoServer.php
+++ src/Ratchet/Server/IoServer.php
@@ -54,9 +54,13 @@ class IoServer {
      * @param  string                             $address    The address to receive sockets on (0.0.0.0 means receive connections from any)
      * @return IoServer
      */
-    public static function factory(MessageComponentInterface $component, $port = 80, $address = '0.0.0.0') {
+    public static function factory(MessageComponentInterface $component, $port = 80, $address = '0.0.0.0', $sslconf = null) {
         $loop   = LoopFactory::create();
         $socket = new Reactor($address . ':' . $port, $loop);
+        
+        if (is_array($sslconf)) {
+            $socket = new SecureReactor($socket, $loop, $sslconf);
+        }
 
         return new static($component, $socket, $loop);
     }
